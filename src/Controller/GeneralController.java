f8033a209a8b31de52fb7d802d7a260aabc2b65a
Mohasin Mia <ehsankhan.cse@gmail.com>
2023-03-27 12:15:08 +0600

    Update GeneralController.java

--- src/Controller/GeneralController.java
+++ src/Controller/GeneralController.java
@@ -1,5 +1,4 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -30,7 +29,7 @@ import javafx.util.Duration;
 
 /**
  * Commonly shared controller variables and methods
- * @author Austin Wong
+ * @author Mohasin Miah
  */
 public class GeneralController {
     
