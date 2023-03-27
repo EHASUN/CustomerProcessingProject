c719a158c256618d353bf4df94f8512c5e26c964
Mohasin Mia <ehsankhan.cse@gmail.com>
2023-03-27 12:18:32 +0600

    Update UpdateCustomerScreenController.java

--- src/Controller/UpdateCustomerScreenController.java
+++ src/Controller/UpdateCustomerScreenController.java
@@ -1,5 +1,4 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -31,7 +30,7 @@ import javafx.scene.input.KeyEvent;
 /**
  * Controls logic for updating customer
  *
- * @author Austin Wong
+ * @author Mohasin Miah
  */
 public class UpdateCustomerScreenController extends GeneralController implements Initializable {
 
