eaa3649ca0b5e07ccbdec1fa482afa58dda0f751
Mohasin Mia <ehsankhan.cse@gmail.com>
2023-03-27 12:18:05 +0600

    Update UpdateAppointmentScreenController.java

--- src/Controller/UpdateAppointmentScreenController.java
+++ src/Controller/UpdateAppointmentScreenController.java
@@ -1,5 +1,4 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -39,7 +38,7 @@ import javafx.scene.input.KeyEvent;
 /**
  * Controls screen for updating existing appointments
  *
- * @author Austin Wong
+ * @author Mohasin Miah
  */
 public class UpdateAppointmentScreenController extends GeneralController implements Initializable {
 
