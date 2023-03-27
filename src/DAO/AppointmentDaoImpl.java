49f97a9e1eea280cc60be560f11ff13c3e508360
Mohasin Mia <ehsankhan.cse@gmail.com>
2023-03-27 12:10:32 +0600

    Update AppointmentDaoImpl.java

--- src/DAO/AppointmentDaoImpl.java
+++ src/DAO/AppointmentDaoImpl.java
@@ -1,5 +1,4 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -26,7 +25,7 @@ import javafx.collections.ObservableList;
 
 /**
  *
- * @author Austin Wong
+ * @author Mohasin Miah
  */
 public class AppointmentDaoImpl extends GeneralDaoImpl{
     
