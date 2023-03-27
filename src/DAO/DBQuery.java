643b516643c4c4913bd8b1a655456e2cfa67c473
Mohasin Mia <ehsankhan.cse@gmail.com>
2023-03-27 12:13:06 +0600

    Update DBQuery.java

--- src/DAO/DBQuery.java
+++ src/DAO/DBQuery.java
@@ -1,5 +1,4 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -11,7 +10,7 @@ import java.sql.SQLException;
 
 /**
  *
- * @author Austin Wong
+ * @author Mohasin Miah
  */
 public class DBQuery {
     
