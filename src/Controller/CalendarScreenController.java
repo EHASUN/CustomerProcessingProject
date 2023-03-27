ec6499845b55b8fca3ce38814c420b2d590580bf
Mohasin Mia <ehsankhan.cse@gmail.com>
2023-03-27 12:14:40 +0600

    Update CalendarScreenController.java

--- src/Controller/CalendarScreenController.java
+++ src/Controller/CalendarScreenController.java
@@ -1,5 +1,4 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -39,7 +38,7 @@ import static DAO.UserDaoImpl.getAllActiveUsers;
 /**
  * Controls calendar logic
  *
- * @author Austin Wong
+ * @author Mohasin Miah
  */
 public class CalendarScreenController extends GeneralController implements Initializable {
 
