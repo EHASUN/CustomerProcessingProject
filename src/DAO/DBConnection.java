22f8e8fd406b5e3e50223b8defa53076bf5a19f9
Mohasin Mia <ehsankhan.cse@gmail.com>
2023-03-27 12:12:40 +0600

    Update DBConnection.java

--- src/DAO/DBConnection.java
+++ src/DAO/DBConnection.java
@@ -1,5 +1,4 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -13,7 +12,7 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author Austin Wong
+ * @author Mohasin Miah
  */
 public class DBConnection {
     
