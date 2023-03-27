c89f7b3540a4c6e37d8fbc58ed46f7ac9aee7fde
Mohasin Mia <ehsankhan.cse@gmail.com>
2023-03-27 12:15:41 +0600

    Update LoginScreenController.java

--- src/Controller/LoginScreenController.java
+++ src/Controller/LoginScreenController.java
@@ -1,5 +1,4 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -28,7 +27,7 @@ import javafx.scene.input.KeyEvent;
 
 /**
  * Handles logic for login screen
- * @author Austin Wong
+ * @author Mohasin Miah
  */
 public class LoginScreenController extends GeneralController implements Initializable {
     
